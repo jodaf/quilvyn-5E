2ee1f0c476c86f141e6b2c49f427ff5c9e633ceb
Jim Hayes <spinaltap12345@gmail.com>
2020-04-03 16:05:26 -0700

    Update version.

--- SRD5E.js
+++ SRD5E.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var SRD5E_VERSION = '1.7.0.0alpha';
+var SRD5E_VERSION = '1.7.0.1alpha';
 
 /*
  * This module loads the rules from Fifth Edition.  The SRD5E function
